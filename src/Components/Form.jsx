5292c916d97477d4cc5e67a7b2d4b4fd41b1e54d
PFJB <jbarbou@outlook.fr>
2023-11-27 20:15:42 +0100

    F01 - main feature

---
+++ src/Components/Form.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function Form() {
+
+    const [input, setInput] = useState("")
+
+    const handleClick = (event) => {
+        event.preventDefault();
+        setInput("");
+        input === "" ? console.error('champ obligatoire') : alert(`Bonjour, ${input}`)
+    }
+
+    const handleChange = (event) => {
+    setInput(event.target.value)
+    }
+
+    return (
+        <div>
+            <h1>Bienvenue chez nous</h1>
+            <h2>Connectez vous ici</h2>
+                <form action="submit">
+                    <input type="text" value={input} placeholder="Entrez votre prénom..." onChange={handleChange}/>
+                    <button onClick={handleClick}>Accédez a votre espace</button>
+                </form>
+        </div>
+    )
+}
