d9a7088a3c95c7342e6c9792d890c3cecc59523b
PFJB <jbarbou@outlook.fr>
2023-11-28 15:27:45 +0100

    F02: ErrorPage > typo

--- src/Components/pages/ErrorPage/ErrorPage.jsx
+++ src/Components/pages/ErrorPage/ErrorPage.jsx
@@ -6,7 +6,7 @@ export default function ErrorPage() {
     <div>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <Link to={"/"}><button>Retourner a la page d'acceuil</button></Link>
+      <Link to={"/"}><button>Retourner a la page d&apos;acceuil</button></Link>
     </div>
   );
 }
