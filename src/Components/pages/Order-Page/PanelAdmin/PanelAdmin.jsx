37b555321c5ccfc00a1b079caece93b7d093d0de
PFJB <jbarbou@outlook.fr>
2023-12-02 15:37:58 +0100

    F07: ORDER > panelAdmin > crea ButtonPanelAdmin + style

--- src/Components/pages/Order-Page/PanelAdmin/PanelAdmin.jsx
+++ src/Components/pages/Order-Page/PanelAdmin/PanelAdmin.jsx
@@ -1,13 +1,21 @@
 import styled from "styled-components";
 import { theme } from "../../../../theme/theme";
-
+import ButtonPanelAdmin from "../../../reusable-ui/button/ButtonPanelAdmin";
+import { GoChevronUp } from "react-icons/go";
+import { FiPlus } from "react-icons/fi";
+import { MdModeEditOutline } from "react-icons/md";
 
 
 
 export default function PanelAdmin() {
   return (
     <PanelAdminStyled>
-              <div className="ButtonZone">Espace button</div>
+              <div className="ButtonZone">
+                <ButtonPanelAdmin icone={<GoChevronUp />}/>
+                <ButtonPanelAdmin icone={<FiPlus />} text={"Ajouter un produit"}/>
+                <ButtonPanelAdmin icone={<MdModeEditOutline />} text={"Modifier un produit"}/>
+
+              </div>
               <div className="ActionZone">Espace Action</div>
     </PanelAdminStyled>
   )
@@ -15,20 +23,25 @@ export default function PanelAdmin() {
 
 const PanelAdminStyled = styled.div`
 
-
       display: flex;
       flex-direction: column;
       position: absolute;
       bottom: 0;
       right: 0;
-      height: 295px;
+      height: fit-content;
       width: 100%;
+
     .ButtonZone {
-      height: 43px;
-      width: 100%;
+        padding-left: 71px;
+        height: 43px;
+        width: fit-content;
+        max-width: 100%;
+
     }
+
     .ActionZone {
       flex: 1;
+      min-height: 250px;
       border: solid 1px ${theme.colors.greyLight};
       background-color: ${theme.colors.white};
       border-radius: 0 0 ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
---
+++ src/Components/reusable-ui/button/ButtonPanelAdmin.jsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { theme } from "../../../theme/theme";
+
+export default function ButtonPanelAdmin({icone, text, className}) {
+  return (
+    <ButtonPanelAdminStyled className={className}>
+        <div>
+            <span>{icone && icone}</span>
+            {text && text}
+        </div>
+    </ButtonPanelAdminStyled>
+  )
+}
+
+const ButtonPanelAdminStyled = styled.button`
+
+    height: 43px;
+    width: fit-content;
+    min-width: 60px;
+    padding: 10px 22px 11px 22px;
+    border: 1px solid ${theme.colors.greyLight} ;
+    border-bottom-width: 2px ;
+    border-radius: 5px 5px 0px 0px;
+    background-color: ${theme.colors.white};
+    box-shadow: 0px -6px 8px -2px #0000001A;
+
+
+
+   
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: fit-content;
+        height: 100%;
+        color: ${theme.colors.greySemiDark};
+        gap: 13px;
+
+        span {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+`;
