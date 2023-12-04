6c023c7cfc546e051cc2cd6cba994ab0a0232433
PFJB <jbarbou@outlook.fr>
2023-12-04 14:05:36 +0100

    F08: MAIN > Add EmptyMenuUsers

--- src/Components/pages/Order-Page/Main/Main.jsx
+++ src/Components/pages/Order-Page/Main/Main.jsx
@@ -4,13 +4,14 @@ import { useContext } from "react";
 import EmptyMenu from "./emptyContent/EmptyMenu";
 import OrderContext from "../../../../context/OrderContext";
 import MenuContent from "./menuContent/MenuContent";
+import EmptyMenuUsers from "./emptyContent/EmptyMenuUsers";
 
 export default function Main() {
 
-  const {menuData} = useContext(OrderContext)
+  const {menuData, IsAdminOn} = useContext(OrderContext)
 
   if ( menuData.length !== 0 ) {return (<Mainstyled><MenuContent/></Mainstyled>)}
-  else {return (<Mainstyled><EmptyMenu /></Mainstyled>)}
+  else {return (<Mainstyled>{IsAdminOn ? <EmptyMenu />: <EmptyMenuUsers/>}</Mainstyled>)}
 }
 
 const Mainstyled = styled.div`
