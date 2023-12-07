8fdb2d158bc1e23f8366bea326d83dd19a38e39a
PFJB <jbarbou@outlook.fr>
2023-12-07 11:43:57 +0100

    F09: ADMIN : retrieved info of product click on with form of AddContent

--- src/Components/pages/Order-Page/PanelAdmin/panelContent/ModifyContent.jsx
+++ src/Components/pages/Order-Page/PanelAdmin/panelContent/ModifyContent.jsx
@@ -1,16 +1,36 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import OrderContext from "../../../../../context/OrderContext"
+import ImagePreview from "./ImagePreview"
+import TextInput from "../../../../reusable-ui/textInput/TextInput"
+import { GetTextInputConfigs } from "./inputTextConfigs"
+import styled from "styled-components"
+import { EMPTY_PRODUCT } from "../../../../../enums/product"
 
 export default function ModifyContent() {
 
   const { setProductSelected, productSelected } = useContext(OrderContext)
+  const textInputs = GetTextInputConfigs(productSelected)
+  const [productBeingEdited, setproductBeingEdited] = useState(EMPTY_PRODUCT)
+
+  const handleChange = (event) => {
+    setproductBeingEdited({ ...productBeingEdited, [event.target.name]: event.target.values })
+    console.log(productBeingEdited)
+  }
 
   return (
-    <div>
-      ModifyProduct
-      <span>{productSelected && productSelected.title}</span>
-    </div>
+    <EditContentStyled>
+      <ImagePreview imageSource={productSelected.imageSource} />
+      <div className="inputArea">
+        {textInputs.map((input) => (
+          <TextInput key={input.id} {...input} onChange={handleChange} version="panelAdmin"
+          />))}
+      </div>
+    </EditContentStyled >
 
   )
 }
+
+const EditContentStyled = styled.form`
+  
+`;
 
