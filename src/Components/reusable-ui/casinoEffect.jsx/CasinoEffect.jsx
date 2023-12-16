0e9f80111205fc0bbd0eac066dfe2a8bc905f913
PFJB <jbarbou@outlook.fr>
2023-12-16 12:45:28 +0100

    F14: CasinoEffect > effect added

--- src/Components/reusable-ui/casinoEffect.jsx/CasinoEffect.jsx
+++ src/Components/reusable-ui/casinoEffect.jsx/CasinoEffect.jsx
@@ -1,5 +1,45 @@
+import styled from "styled-components";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 export default function CasinoEffect({ count, className }) {
     return (
-        <span className={className}>{count}</span>
+        <TransitionGroup component={CasinoEffectStyled} className={"transition-group"}>
+            <CSSTransition classNames={"price-animated"} timeout={500} key={count}>
+                <span className={className}>
+                    {count}
+                </span>
+            </CSSTransition>
+        </TransitionGroup>
     )
 }
+
+const CasinoEffectStyled = styled.div`
+    position: relative;
+    overflow-y: hidden;
+  
+  span{
+    display: inline-block;
+  }
+
+  .price-animated-enter{
+    transform: translateY(100%);
+  }
+
+  .price-animated-enter-active{
+    transform: translateY(0%);    
+    transition: all 0.5s;
+  }
+
+  .price-animated-exit{
+    transform: translateY(0%);
+    position :absolute;
+    right: 0;
+    bottom: 0;
+  }
+
+  .price-animated-exit-active{
+    transform: translateY(-100%);
+    transition: all 0.5s;
+  }
+
+`;
