164aa3e4d48613ca39e2687ca51af3d60d04fc3a
PFJB <jbarbou@outlook.fr>
2023-12-19 19:49:01 +0100

    F15: ImagePreview > refacto

--- src/Components/pages/Order-Page/PanelAdmin/panelContent/adminForm/ImagePreview.jsx
+++ src/Components/pages/Order-Page/PanelAdmin/panelContent/adminForm/ImagePreview.jsx
@@ -3,13 +3,14 @@ import { theme } from "../../../../../../theme/theme";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { panelImagePreviewAnimation } from "../../../../../../theme/animation";
 
-export default function ImagePreview({ imageSource }) {
+export default function ImagePreview({ imageSource, title = "Added picture" }) {
     return (
         <TransitionGroup component={ImagePreviewStyled}>
             <CSSTransition classNames={"image"} appear={true} key={imageSource} timeout={{ enter: 500 }}>
-                <div className="imageArea">
-                    {imageSource ? <img className="img" src={imageSource} alt="Added picture" /> : "Aucune image"}
-                </div>
+                {imageSource ?
+                    <img className="img" src={imageSource} alt={title} />
+                    :
+                    <p className="aucuneImage">Aucune image</p>}
             </CSSTransition>
         </TransitionGroup>
     )
@@ -17,28 +18,26 @@ export default function ImagePreview({ imageSource }) {
 
 const ImagePreviewStyled = styled.div`
 
-    display: flex;
-    justify-content: center;
-    align-items: center;
     grid-area: 1 / 1 / 4 / 2;
-    border: solid 1px ${theme.colors.greyLight};
-    border-radius: ${theme.borderRadius.round};
-    width: 160px;
-    height: 120px;
-    font-size: ${theme.fonts.size.P0};
-    font-weight: ${theme.fonts.weights.regular};
-    font-family: 'Open Sans', sans-serif;
-    color: ${theme.colors.greyMedium};
 
-    .imageArea{
-        height: 100%;
+    & > *, &, img{
         width: 100%;
-        
-        .img {
-            width: 100%;
-            height: 100%;
-            object-fit: contain;
-        }
+        height: 100%;
+    }
+
+    .aucuneImage{
+        display: grid;
+        place-content: center;
+        border: solid 1px ${theme.colors.greyLight};
+        border-radius: ${theme.borderRadius.round};
+        font-size: ${theme.fonts.size.P0};
+        font-weight: ${theme.fonts.weights.regular};
+        font-family: 'Open Sans', sans-serif;
+        color: ${theme.colors.greyMedium};
+    }
+
+    .img{
+        object-fit: contain; 
     }
   
     ${panelImagePreviewAnimation}
